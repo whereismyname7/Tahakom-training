90963f7e3e473cd2ec47e1b227a60c2f8b5cf985
whereismyname7 <kalshmrani70@hotmail.com>
2024-07-08 10:00:56 +0300

    feat: arabic language + rtl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'skills-assessment';
+
+  //public? is it a good approch
+  constructor(private router: Router,public translate: TranslateService) {
+    this.translate.addLangs(['en', 'ar']);
+    this.translate.setDefaultLang('en');
+
+    const browserLang = this.translate.getBrowserLang();
+    this.translate.use(browserLang && browserLang.match(/en|ar/) ? browserLang : 'en');
+  }
+
+  showLanguageButtons(): boolean {
+    return this.router.url == '/'; 
+  }
+
+  switchLang(lang: string) {
+    this.translate.use(lang);
+  }
 }
